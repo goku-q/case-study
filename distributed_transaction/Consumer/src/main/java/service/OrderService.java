a200cd7fcf1205c14ffef519fc9a98fe7ef92df6
goku-q <goku-q@163.com>
2020-07-03 14:47:34 +0800

    基于RabbitMQ的消息最终一致性实现分布式事务

--- distributed_transaction/Consumer/src/main/java/service/OrderService.java
+++
@@ -1,5 +0,0 @@
-package service;
-
-public interface OrderService {
-	public void showUser();
-}
--- distributed_transaction/Interface/src/main/java/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package service;
-
-
-import pojo.User;
-
-public interface UserService {
-	//需要暴露的服务
-	public User getUser();
-}
